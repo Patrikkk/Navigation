f5b8d138960ecfe6f261329dbc5cec67bb953ced
Zaicon Kiroshu <kzaicon@yahoo.com>
2017-12-11 09:54:01 -0600

    Initial Commit

---
+++ Navigation/NavMain.cs
@@ -0,0 +1,199 @@
+﻿/*
+ * Yes, this is extracted from the original Essentials plugin.
+ * Credit goes to the original creators of these commands.
+ * 
+ */
+
+using System;
+using System.Reflection;
+using Terraria;
+using TerrariaApi.Server;
+using TShockAPI;
+
+namespace Navigation
+{
+	[ApiVersion(2,1)]
+    public class NavMain : TerrariaPlugin
+    {
+		#region PluginInfo
+		public override string Author => "Zaicon";
+		public override string Description => "Basic navigational commands for tShock.";
+		public override string Name => "Navigation";
+		public override Version Version => Assembly.GetExecutingAssembly().GetName().Version;
+		#endregion
+
+		#region Initialize/Dispose
+		public override void Initialize()
+		{
+			ServerApi.Hooks.GameInitialize.Register(this, OnInitialize);
+		}
+
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing)
+			{
+				ServerApi.Hooks.GameInitialize.Deregister(this, OnInitialize);
+			}
+			base.Dispose(disposing);
+		}
+		#endregion
+
+		public NavMain(Main game) : base (game)
+		{
+
+		}
+
+		#region Hooks
+		private void OnInitialize(EventArgs args)
+		{
+			Commands.ChatCommands.Add(new Command("nagivation.top", Top, "top"));
+			Commands.ChatCommands.Add(new Command("nagivation.up", Up, "up"));
+			Commands.ChatCommands.Add(new Command("nagivation.down", Down, "down"));
+			Commands.ChatCommands.Add(new Command("nagivation.left", Left, "left"));
+			Commands.ChatCommands.Add(new Command("nagivation.right", Right, "right"));
+		}
+		#endregion
+
+		#region Commands
+		private void Top(CommandArgs args)
+		{
+			int Y = Utils.GetTop(args.Player.TileX);
+			if (Y == -1)
+			{
+				args.Player.SendErrorMessage("You are already on the top.");
+				return;
+			}
+			args.Player.Teleport(args.Player.TileX * 16F, Y * 16F);
+			args.Player.SendSuccessMessage("Teleported you to the top.");
+		}
+
+		private void Up(CommandArgs args)
+		{
+			int levels = 1;
+			if (args.Parameters.Count > 0 && !int.TryParse(args.Parameters[0], out levels))
+			{
+				args.Player.SendErrorMessage($"Usage: {TShock.Config.CommandSpecifier}up [No. levels]");
+				return;
+			}
+
+			int Y = Utils.GetUp(args.Player.TileX, args.Player.TileY);
+			if (Y == -1)
+			{
+				args.Player.SendErrorMessage("You are already on the top.");
+				return;
+			}
+			bool limit = false;
+			for (int i = 1; i < levels; i++)
+			{
+				int newY = Utils.GetUp(args.Player.TileX, Y);
+				if (newY == -1)
+				{
+					levels = i;
+					limit = true;
+					break;
+				}
+				Y = newY;
+			}
+
+			args.Player.Teleport(args.Player.TileX * 16F, Y * 16F);
+			args.Player.SendSuccessMessage($"Teleported you up {levels} level(s). {(limit ? "You can't go up any further." : string.Empty)}");
+		}
+
+		private void Down(CommandArgs args)
+		{
+			int levels = 1;
+			if (args.Parameters.Count > 0 && !int.TryParse(args.Parameters[0], out levels))
+			{
+				args.Player.SendErrorMessage($"Usage: {TShock.Config.CommandSpecifier}down [No. levels]");
+				return;
+			}
+
+			int Y = Utils.GetDown(args.Player.TileX, args.Player.TileY);
+			if (Y == -1)
+			{
+				args.Player.SendErrorMessage("You are already on the bottom.");
+				return;
+			}
+			bool limit = false;
+			for (int i = 1; i < levels; i++)
+			{
+				int newY = Utils.GetDown(args.Player.TileX, Y);
+				if (newY == -1)
+				{
+					levels = i;
+					limit = true;
+					break;
+				}
+				Y = newY;
+			}
+
+			args.Player.Teleport(args.Player.TileX * 16F, Y * 16F);
+			args.Player.SendSuccessMessage($"Teleported you down {levels} level(s). {(limit ? "You can't go down any further." : string.Empty)}");
+		}
+
+		private void Left(CommandArgs args)
+		{
+			int levels = 1;
+			if (args.Parameters.Count > 0 && !int.TryParse(args.Parameters[0], out levels))
+			{
+				args.Player.SendErrorMessage("Usage: /left [No. times]");
+				return;
+			}
+
+			int X = Utils.GetLeft(args.Player.TileX, args.Player.TileY);
+			if (X == -1)
+			{
+				args.Player.SendErrorMessage("You cannot go any further left.");
+				return;
+			}
+			bool limit = false;
+			for (int i = 1; i < levels; i++)
+			{
+				int newX = Utils.GetLeft(X, args.Player.TileY);
+				if (newX == -1)
+				{
+					levels = i;
+					limit = true;
+					break;
+				}
+				X = newX;
+			}
+
+			args.Player.Teleport(X * 16F, args.Player.TileY * 16F);
+			args.Player.SendSuccessMessage($"Teleported you to the left {levels} time(s). {(limit ? "You can't go any further." : string.Empty)}");
+		}
+
+		private void Right(CommandArgs args)
+		{
+			int levels = 1;
+			if (args.Parameters.Count > 0 && !int.TryParse(args.Parameters[0], out levels))
+			{
+				args.Player.SendErrorMessage($"Usage: {TShock.Config.CommandSpecifier}right [No. times]");
+				return;
+			}
+
+			int X = Utils.GetRight(args.Player.TileX, args.Player.TileY);
+			if (X == -1)
+			{
+				args.Player.SendErrorMessage("You cannot go any further right.");
+				return;
+			}
+			bool limit = false;
+			for (int i = 1; i < levels; i++)
+			{
+				int newX = Utils.GetRight(X, args.Player.TileY);
+				if (newX == -1)
+				{
+					levels = i;
+					limit = true;
+					break;
+				}
+				X = newX;
+			}
+
+			args.Player.Teleport(X * 16F, args.Player.TileY * 16F);
+			args.Player.SendSuccessMessage($"Teleported you to the right {levels} time(s). {(limit ? "You can't go any further." : string.Empty)}");
+		}
+		#endregion
+	}
+}
---
+++ Navigation/Utils.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+
+namespace Navigation
+{
+	public static class Utils
+	{
+		public static int GetTop(int TileX)
+		{
+			for (int y = 0; y < Main.maxTilesY; y++)
+			{
+				if (WorldGen.SolidTile(TileX, y))
+					return y - 3;
+			}
+			return -1;
+		}
+
+		public static int GetUp(int TileX, int TileY)
+		{
+			for (int y = TileY - 1; y > 0; y--)
+			{
+				if (WorldGen.SolidTile(TileX, y + 1) && !WorldGen.SolidTile(TileX, y) && !WorldGen.SolidTile(TileX, y - 1) && !WorldGen.SolidTile(TileX, y - 2))
+					return y - 2;
+			}
+			return -1;
+		}
+
+		public static int GetDown(int TileX, int TileY)
+		{
+			for (int y = TileY + 3; y < Main.maxTilesY; y++)
+			{
+				if (WorldGen.SolidTile(TileX, y + 1) && !WorldGen.SolidTile(TileX, y) && !WorldGen.SolidTile(TileX, y - 1) && !WorldGen.SolidTile(TileX, y - 2))
+					return y - 2;
+			}
+			return -1;
+		}
+
+		public static int GetLeft(int TileX, int TileY)
+		{
+			bool FoundSolid = false;
+			for (int x = TileX; x > 0; x--)
+			{
+				if (!FoundSolid)
+				{
+					if (!WorldGen.SolidTile(x, TileY))
+						continue;
+					else
+						FoundSolid = true;
+				}
+				else if (!WorldGen.SolidTile(x - 1, TileY) &&
+					!WorldGen.SolidTile(x, TileY + 1) && !WorldGen.SolidTile(x - 1, TileY + 1) &&
+					!WorldGen.SolidTile(x, TileY + 2) && !WorldGen.SolidTile(x - 1, TileY + 2))
+					return x - 1;
+				else
+					FoundSolid = true;
+			}
+			return -1;
+		}
+
+		public static int GetRight(int TileX, int TileY)
+		{
+			bool FoundSolid = false;
+			for (int x = TileX + 1; x < Main.maxTilesX; x++)
+			{
+				if (!FoundSolid)
+				{
+					if (!WorldGen.SolidTile(x, TileY))
+						continue;
+					else
+						FoundSolid = true;
+				}
+				else if (!WorldGen.SolidTile(x + 1, TileY) &&
+					!WorldGen.SolidTile(x, TileY + 1) && !WorldGen.SolidTile(x + 1, TileY + 1) &&
+					!WorldGen.SolidTile(x, TileY + 2) && !WorldGen.SolidTile(x + 1, TileY + 2))
+					return x + 1;
+			}
+			return -1;
+		}
+	}
+}
